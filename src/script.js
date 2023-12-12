86a063c115a196587117410baddfa056437c0c79
Replit user <>
2023-12-12 17:36:17 +0000

    Made a Find Synonyms app, ready for deployment

---
+++ src/script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("input")
+const url = "https://api.datamuse.com/words?rel_syn="
+const list = document.getElementById("list")
+const btn = document.getElementById("search-btn")
+const del = document.getElementById("delete")
+const info = document.getElementById("info")
+
+async function getSynonyms() {
+  try {
+    const endpoint = url + input.value
+    const response = await fetch(endpoint, {
+      cache: 'no-cache'
+    })
+    if (response.status === 200) {
+      const JSONSynonyms = await response.json()
+      info.innerHTML = `Synonyms to ${input.value}:`
+      JSONSynonyms.forEach(item => {
+        const li = document.createElement("li")
+
+        li.textContent = item.word
+
+        list.append(li)
+      })
+    }
+  } catch(error) {
+    alert("An error occurred!")
+  }
+}
+
+function whenRemove() {
+  list.innerHTML = ''
+}
+
+btn.addEventListener("click", getSynonyms)
+del.addEventListener("click", whenRemove)
